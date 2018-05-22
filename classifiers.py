b9c118928e4526a25cba2e35bdbea05970352b1e
kotek <kotekmath@gmail.com>
2018-05-22 15:23:36 +0300

    asd

--- classifiers.py
+++ classifiers.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -10,24 +11,28 @@ def main():
 
     #for i in range(10):
     train_set, test_set = train_test_split(df, test_size=1000 / df.shape[0])
-    y_train_set = train_set.iloc[:, -1]
+    train_set_label = train_set.iloc[:, -1]
+    test_set_label = test_set.iloc[:, -1]
 
     #  learn train_set, predict test_set
-    logistic_reg.fit(train_set, y_train_set)
-    y_test_predicts = np.argsort(logistic_reg.predict_proba(test_set))
+    logistic_reg.fit(train_set, train_set_label)
+    test_set_predicts = logistic_reg.predict_proba(test_set)
+    sorted_arr = np.sort(test_set_predicts, axis=0)
 
-    print(y_test_predicts)
+    NP = test_set_label.sum()  # all labels which are 1 == sum
 
-    # logistic_reg.fit(train_set)
-    # print(logistic_reg.predict_proba(test_set))
-    # test_set = df.sample(n= 1000, axis=0)
-    # print(test_set)
-    # print(test_set.shape)
-    # for x in range(1, 100):
-    #     rows = np.random.rand(len(df)) < x / 100
-    #     train = df[rows]
-    #     test = df[~rows]
-    pass
+    # init both arrays
+    Ni = np.zeros(NP+1) # Ni == TPR
+    FPR = np.zeros(NP+1)
 
+    print(test_set_label)
+  #  for i in range(NP):
+
+
+
+
+    # plt.plot(FPR, Ni, label="ROC CURVE")
+    # plt.legend()
+    # plt.show()
 
 if __name__ == '__main__':  main()
